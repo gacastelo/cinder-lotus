fe2e4fb6c51984a41582e1af9196e4cc4634648a
gacastelo <gabriel.castelo5949@gmail.com>
2026-09-20 21:44:34 -0300

    fix: redirect vitoria.php

--- public/mapa.php
+++ public/mapa.php
@@ -40,13 +40,13 @@ if (isset($_GET["item"])) {
     exit();
 }
 
-//for ($i = 2; $i < 12; $i++) {
-//    CenarioService::unlockNextLevel($i);
-//}
+for ($i = 2; $i < 12; $i++) {
+    CenarioService::unlockNextLevel($i);
+}
 
 if (isset($_SESSION["matouBossFinal"])) {
     if ($_SESSION["matouBossFinal"]) {
-        header("Location: fim.php");
+        header("Location: vitoria.php");
         exit();
     }
 }
